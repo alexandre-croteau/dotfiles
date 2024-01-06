61ed7a3ff548924accf4eeafde7058ae04b06905
Alexandre Croteau <5752494+alexandre-croteau@users.noreply.github.com>
2024-01-05 20:21:04 -0500

    feat: nvim

---
+++ config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,9 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local keymap = vim.keymap
+
+local opts = { noremap = true, silent = true }
+
+-- keymap.set("i", "A-<tab>", "<leader><bs>w", opts)
