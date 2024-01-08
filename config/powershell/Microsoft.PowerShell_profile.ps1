5b7f3b1cf0723a6c42602687b1cb41e88fd188e9
Alexandre Croteau <5752494+alexandre-croteau@users.noreply.github.com>
2024-01-08 15:54:18 -0500

    fix: update powershell

--- config/powershell/Microsoft.PowerShell_profile.ps1
+++ config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,9 @@
-oh-my-posh init pwsh --config "/usr/local/opt/oh-my-posh/themes/hotstick.minimal.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "../oh-my-posh/default.omp.json" | Invoke-Expression
+
+function Open-Eza { & eza --long --header --icons --all }
+New-Alias -Name "la" -Value "Open-Eza" -Description "eza aka exa" -Option ReadOnly -Force
+New-Alias -Name "ll" -Value "Open-Eza" -Description "eza aka exa" -Option ReadOnly -Force
+
+function Open-Neovim([string]$FileName) { & nvim $FileName }
+New-Alias -Name "vi" -Value "Open-Neovim" -Description "neovim" -Option ReadOnly -Force
+New-Alias -Name "vim" -Value "Open-Neovim" -Description "neovim" -Option ReadOnly -Force
