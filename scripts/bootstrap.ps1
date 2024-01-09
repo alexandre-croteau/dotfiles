d628d45f27c94da805fd53ac88be30fda38a15b2
Alexandre Croteau <5752494+alexandre-croteau@users.noreply.github.com>
2024-01-08 20:32:09 -0500

    fix: only run on Windows

--- scripts/bootstrap.ps1
+++ scripts/bootstrap.ps1
@@ -1,15 +1,16 @@
-### Powershell
-# https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_profiles?view=powershell-7.4#profile-types-and-locations
-New-Item -Path $PROFILE -ItemType SymbolicLink -Value "$env:USERPROFILE\.dotfiles\config\powershell\Microsoft.PowerShell_profile.ps1" -Force
-# TODO MacOS/Linux: New-Item -Path "~/.config/powershell/Microsoft.PowerShell_profile.ps1 -ItemType SymbolicLink -Value "$env:USERPROFILE\.dotfiles\config\powershell\Microsoft.PowerShell_profile.ps1"
+if ($IsWindows) {
+  ### Powershell
+  # https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_profiles?view=powershell-7.4#profile-types-and-locations
+  New-Item -Path $PROFILE -ItemType SymbolicLink -Value "$env:USERPROFILE\.dotfiles\config\powershell\Microsoft.PowerShell_profile.ps1" -Force
 
-### Scoop
-Move-Item $env:USERPROFILE\.config\scoop $env:USERPROFILE\.config\scoop.bak -Force
-New-Item -Path "$env:USERPROFILE\.config\scoop" -ItemType SymbolicLink -Value "$env:USERPROFILE\.dotfiles\config\scoop" -Force
+  ### Scoop
+  Move-Item $env:USERPROFILE\.config\scoop $env:USERPROFILE\.config\scoop.bak -Force
+  New-Item -Path "$env:USERPROFILE\.config\scoop" -ItemType SymbolicLink -Value "$env:USERPROFILE\.dotfiles\config\scoop" -Force
 
-### Neovim
-# 1. Install LazyVim https://www.lazyvim.org/installation
-# Move-Item $env:LOCALAPPDATA\nvim $env:LOCALAPPDATA\nvim.bak -Force
-# Move-Item $env:LOCALAPPDATA\nvim-data $env:LOCALAPPDATA\nvim-data.bak -Force
+  ### Neovim
+  # 1. Install LazyVim https://www.lazyvim.org/installation
+  # Move-Item $env:LOCALAPPDATA\nvim $env:LOCALAPPDATA\nvim.bak -Force
+  # Move-Item $env:LOCALAPPDATA\nvim-data $env:LOCALAPPDATA\nvim-data.bak -Force
 
-New-Item -Path "$env:LOCALAPPDATA\nvim" -ItemType SymbolicLink -Value "$env:USERPROFILE\.dotfiles\config\nvim" -Force
+  New-Item -Path "$env:LOCALAPPDATA\nvim" -ItemType SymbolicLink -Value "$env:USERPROFILE\.dotfiles\config\nvim" -Force
+}
