806e34b8a89ef16da05ec45bb8f6ebf3940ccc5a
Alexandre Croteau <5752494+alexandre-croteau@users.noreply.github.com>
2024-01-10 22:42:29 -0500

    feat: update nvim

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,9 +1,25 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
+-- Add any additional keymaps hereby
 local keymap = vim.keymap
-
 local opts = { noremap = true, silent = true }
 
--- keymap.set("i", "A-<tab>", "<leader><bs>w", opts)
+-- Delete some pre-builds keys from lazyvim
+keymap.del("n", "<C-s>")
+keymap.del("n", "<leader>e")
+
+--Delete with x does not copy to clipboard
+keymap.set("n", "x", '"_x')
+-- Save file using leader w
+keymap.set("n", "<leader>w", ":w<cr>", opts)
+-- Open neo tree (sidepart files)
+keymap.set("n", "<C-n>", ":Neotree<cr>", opts)
+-- Keep just current buffer opened
+keymap.set("n", "<leader>to", ":BufferLineCloseOthers<cr>", opts)
+-- Adding a new tab (file)
+keymap.set("n", "<leader>tn", ":tabnew<cr>", opts)
+-- Select all file
+keymap.set("n", "va", "<esc>ggVG", opts)
+
+-- Add a line bellow without put cursor down
+keymap.set("n", "<leader>o", "printf('m`%so<ESC>``', v:count2)", opts)
