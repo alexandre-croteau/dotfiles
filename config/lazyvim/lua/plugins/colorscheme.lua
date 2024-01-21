14d219952404771833d8d1d8ee3094df348b0370
Alexandre Croteau <5752494+alexandre-croteau@users.noreply.github.com>
2024-01-20 23:47:28 -0500

    fix(lazyvim): config copilot.lua

--- config/lazyvim/lua/plugins/colorscheme.lua
+++ config/lazyvim/lua/plugins/colorscheme.lua
@@ -1,19 +1,14 @@
--- if true then return {} end
-
 return {
   {
     "rebelot/kanagawa.nvim",
     name = "kanagawa",
-    -- enabled = false,
   },
   {
     "catppuccin/nvim",
     name = "catppuccin",
-    -- enabled = false,
   },
   {
     "LazyVim/LazyVim",
-    -- enabled = false,
     opts = {
       colorscheme = "kanagawa-dragon", --"catppuccin-mocha",
     },
--- config/lazyvim/lua/plugins/copilot.lua
+++ config/lazyvim/lua/plugins/copilot.lua
@@ -21,10 +21,37 @@ return {
     "zbirenbaum/copilot.lua",
     name = "copilot.lua",
     lazy = false,
-    enabled = true,
+    -- enabled = true,
     cmd = "Copilot",
     event = "InsertEnter",
     opts = {
+      panel = {
+        enabled = true,
+        -- auto_refresh = false,
+        -- keymap = {
+        --   jump_prev = "[[",
+        --   jump_next = "]]",
+        --   accept = "<CR>",
+        --   refresh = "gr",
+        --   open = "<M-CR>",
+        -- },
+        layout = {
+          position = "right", -- | top | left | right
+          --   ratio = 0.4,
+        },
+      },
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        keymap = {
+          accept = "<Tab>",
+          accept_word = "<S-Tab>",
+          accept_line = "<Tab>",
+          next = "<M-Right>",
+          prev = "<M-Left>",
+          dismiss = "<M-Down>",
+        },
+      },
       filetypes = {
         ["*"] = true,
         plaintext = false,
