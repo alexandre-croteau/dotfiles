4d9f08a4e1daef3f56b1787d310413c69a94c678
Alexandre Croteau <5752494+alexandre-croteau@users.noreply.github.com>
2024-03-04 13:34:14 -0500

    chore(powershell): update config

--- script/setup.ps1
+++ script/setup.ps1
@@ -4,7 +4,7 @@ if ($IsWindows) {
   New-Item -Path $PROFILE -ItemType SymbolicLink -Value "$env:USERPROFILE\.dotfiles\config\powershell\Microsoft.PowerShell_profile.ps1" -Force
 
   ### Scoop
-  Move-Item $env:USERPROFILE\.config\scoop $env:USERPROFILE\.config\scoop.bak -Force
+  # Move-Item $env:USERPROFILE\.config\scoop $env:USERPROFILE\.config\scoop.bak -Force
   New-Item -Path "$env:USERPROFILE\.config\scoop" -ItemType SymbolicLink -Value "$env:USERPROFILE\.dotfiles\config\scoop" -Force
 
   ### Lazyvim
@@ -13,8 +13,25 @@ if ($IsWindows) {
   # Move-Item $env:LOCALAPPDATA\nvim-data $env:LOCALAPPDATA\nvim-data.bak -Force
   New-Item -Path "$env:LOCALAPPDATA\nvim" -ItemType SymbolicLink -Value "$env:USERPROFILE\.dotfiles\config\lazyvim" -Force
 
+  ### Git
+	if (-not (Test-Path "$env:USERPROFILE\.dotfiles\git\gitconfig.symlink")) {
+		Write-Host " - what is your github author name?"
+		$git_authorname = Read-Host
+		Write-Host " - what is your github author email?"
+		$git_authoremail = Read-Host
+		Write-Host " - what is your signing key?"
+		$git_signingkey = Read-Host
+
+		(Get-Content "$env:USERPROFILE\.dotfiles\git\gitconfig.symlink.example") `
+      -replace "GIT_AUTHORNAME", $git_authorname `
+			-replace "GIT_AUTHOREMAIL", $git_authoremail `
+			-replace "GIT_SIGNINGKEY", $git_signingkey | Set-Content "$env:USERPROFILE\.dotfiles\git\gitconfig.symlink"
+	}
+
+  New-Item -Path "$env:USERPROFILE\.gitconfig" -ItemType SymbolicLink -Value "$env:USERPROFILE\.dotfiles\git\gitconfig.symlink" -Force
+
   ### Lazygit
   # https://github.com/jesseduffield/lazygit
-  Move-Item $env:APPDATA\lazygit $env:APPDATA\lazygit.bak -Force
+  # Move-Item $env:APPDATA\lazygit $env:APPDATA\lazygit.bak -Force
   New-Item -Path "$env:APPDATA\lazygit" -ItemType SymbolicLink -Value "$env:USERPROFILE\.dotfiles\config\lazygit" -Force
 }
