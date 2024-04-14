832cb3d59359af72ea22aa572054c3679db453f2
Alexandre Croteau <5752494+alexandre-croteau@users.noreply.github.com>
2024-04-14 00:14:57 -0400

    chore(lazyvim): disable auto config for lazygit

--- config/lazyvim/lua/config/options.lua
+++ config/lazyvim/lua/config/options.lua
@@ -2,6 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.g.lazygit_config = false
+
 -- Github Copilot
 --vim.g.copilot_no_tab_map = true
 --vim.g.copilot_assume_mapped = true
