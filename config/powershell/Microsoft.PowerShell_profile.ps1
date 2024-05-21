81824a856aefe5b93520cd5256193df2bb81b1ee
Alexandre Croteau <5752494+alexandre-croteau@users.noreply.github.com>
2024-05-21 11:45:17 -0400

    fix(pwsh): handle null or empty path on vi/vim alias

--- config/powershell/Microsoft.PowerShell_profile.ps1
+++ config/powershell/Microsoft.PowerShell_profile.ps1
@@ -4,7 +4,7 @@ function Open-Eza { & eza --long --header --icons --all --sort=name --group-dire
 New-Alias -Name "la" -Value "Open-Eza" -Description "Eza" -Option ReadOnly -Force
 New-Alias -Name "ll" -Value "Open-Eza" -Description "Eza" -Option ReadOnly -Force
 
-function Open-Neovim([string]$FileName) { & nvim $FileName }
+function Open-Neovim([string]$FileName) { if ( $FileName ) { & nvim $FileName } else { & nvim } }
 New-Alias -Name "vi" -Value "Open-Neovim" -Description "Neovim" -Option ReadOnly -Force
 New-Alias -Name "vim" -Value "Open-Neovim" -Description "Neovim" -Option ReadOnly -Force
 
